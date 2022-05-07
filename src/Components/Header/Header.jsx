b0c3b0213f4f238c8bdc691ec8ab9849bfdf86a5
TemitopeAsama <temitopeasama@gmail.com>
2022-05-07 10:02:03 +0100

    linked heading to homepage

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import DarkMode from './DarkMode'
 
 const Header = ({ switchTheme, buttonText, buttonIcon }) => {
 	return (
 		<header>
-			<h2 role="heading">Where in the world?</h2>
+			<Link
+				to={{
+					pathname: "/",
+					search: "",
+					state: {},
+				}}
+			>
+				<h2 role='heading'>Where in the world?</h2>
+			</Link>
 			<DarkMode
 				switchTheme={switchTheme}
 				buttonText={buttonText}
